07faf46bb2200c5d66533c6d069adeac87fa4e4f
Benjamin Guedj <74671966+GuedjCode@users.noreply.github.com>
2022-11-30 19:15:09 +0200

    Places App Upload.

---
+++ server.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+//static path
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use(express.static(path.join("public")));
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+//   next();
+// });
+
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
+
+//static files
+app.use((req, res, next) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
+// //error routes.
+// app.use((req, res, next) => {
+//   const error = new HttpError("Could not find this route.", 404);
+//   throw error;
+// });
+
+//error handler.
+app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+//connect to MongoDB and server.
+const PORT = 5000;
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@places-app.ilaed1o.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(process.env.PORT || PORT, () => {
+      console.log("=========================================================");
+      console.log("MongoDB Connected & server listening on port " + PORT);
+      console.log("=========================================================");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
